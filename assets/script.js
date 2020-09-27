8434c274fe20d1e1ff19fc59d363f51d8abbf4d9
geigea84 <geigea84@gmail.com>
2020-09-26 23:18:55 -0500

    added id for individual sections to contain individual questions

--- assets/script.js
+++ assets/script.js
@@ -16,55 +16,54 @@ var questions = [
         answer:"c"
     }
 ];
-var questionsList = document.querySelector(".questions-list");
 var titlePage = document.querySelector(".title-page");
 var pageContent = document.querySelector(".page-content");
-var questionTitle = document.querySelector("#question-title");
-var choiceList = document.querySelector("#choice-list");
+var questionWrapper1 = document.querySelector("#question-wrapper-1");
 
 //function to start quiz and timer
 var startQuiz = function() {
     console.log("startQuiz works");
 
-    //remove previous html
-    titlePage.remove();
-
     //start timer
 
     //move all of the following insert stuff to next function
-    //adding array to html as list items
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "question-item";
-    console.log(listItemEl);
-
-    //adding div to li to hold question info
-    var questionInfoEl = document.createElement("div");
-    questionInfoEl.className = "question-info";
-    console.log(questionInfoEl);
     
     //adding array to div inside li
-    questionInfoEl.innerHTML = 
-    "<h2 id ='question-title'>" + questions[0].title + "</h2>" +
-    "<button id ='choice-list'>" + questions[0].choices[0] + "</button>" +
-    "<button id ='choice-list'>" + questions[0].choices[1] + "</button>" +
-    "<button id ='choice-list'>" + questions[0].choices[2] + "</button>" +
-    "<button id ='choice-list'>" + questions[0].choices[3] + "</button>";
-    console.log(questionInfoEl.innerHTML);
+    //questionInfoEl.innerHTML = 
+    //"<h2 id ='question-title'>" + questions[0].title + "</h2>" +
+    //"<button id ='choice-list'>" + questions[0].choices[0] + "</button>" +
+    //"<button id ='choice-list'>" + questions[0].choices[1] + "</button>" +
+    //"<button id ='choice-list'>" + questions[0].choices[2] + "</button>" +
+    //"<button id ='choice-list'>" + questions[0].choices[3] + "</button>";
+    //console.log(questionInfoEl.innerHTML);
 
     //adding array to page
-    listItemEl.appendChild(questionInfoEl);
-    questionsList.appendChild(listItemEl);
-    console.log(listItemEl.appendChild);
-    console.log(questionsList.appendChild);
+    //listItemEl.appendChild(questionInfoEl);
+    //questionsList.appendChild(listItemEl);
+    //console.log(listItemEl.appendChild);
+    //console.log(questionsList.appendChild);
 
-    clickAnswerButton();
+    clickAnswerButton1();
 };
 
 //function to select answers in quiz
-var clickAnswerButton = function() {
+var clickAnswerButton1 = function() {
     console.log("clickAnswerButton works");
 
+    //remove previous html
+    titlePage.remove();
 
+    //insert array info
+    questionWrapper1.innerHTML = 
+    "<h2 class ='question-title'>" + questions[0].title + "</h2>" +
+    "<button class ='choice-list'>" + questions[0].choices[0] + "</button>" +
+    "<button class ='choice-list'>" + questions[0].choices[1] + "</button>" +
+    "<button class ='choice-list'>" + questions[0].choices[2] + "</button>" +
+    "<button class ='choice-list'>" + questions[0].choices[3] + "</button>";
+    
+    if (questions[0].answer.clicked === true) {
+        alert("correct button clicked");
+    }
 };
 
 //function to
