ab39c01cd41b40b8cdc85097390aee572b1b2ea6
Geiger <geiga@shopbop.com>
2021-01-11 14:47:31 -0600

    updated html layout, reformatted functions, more emphasis on pseudocode

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -10,29 +10,60 @@ let questions = [
         answer: "b"
     },
     {
-        title: "testing: click response a",
+        title: "testing: click response c",
         choices: ["a", "b", "c", "d"],
         answer: "a"
     }
 ];
 
 let time = 40
+let timerId;
 let questionIndex = 0;
 
 const startEl = document.querySelector("#start");
-const mainEl = document.querySelector("#main");
+const questionsEl = document.querySelector("#questions");
+const finishEl = document.querySelector("#finish");
 const timerEl = document.querySelector("#timer");
-
+const questionTitle = document.querySelector("#question-title");
+const responseOptions = document.querySelector("#response-options");
 
 function startQuiz() {
     startEl.setAttribute("class", "hide");
-    mainEl.removeAttribute("class", "hide");
-    timerEl.innerHTML(time);
+    questionsEl.removeAttribute("class");
 
+    //start timer
+    timerEl.textContent = time;
+    timerId = setInterval(timerCountdown, 1000);
+
+    startQuestions();
 }
 
 function startQuestions() {
+    //questionsEl.textContent = "";
+    let currentQuestion = questions[questionIndex]
+
+    questionTitle.textContent = currentQuestion.title;
+    responseOptions.textContent = currentQuestion.choices;
+
+    
+}
+
+function checkQuestion() {
 
 }
 
-document.querySelector("#start-quiz").addEventListener("click", startQuiz);
+function endQuiz() {
+    questionsEl.setAttribute("class", "hide");
+    finishEl.removeAttribute("class");
+}
+
+function timerCountdown() {
+    time--;
+    timerEl.textContent = time;
+
+    if(time <= 0) {
+        endQuiz();
+    }
+}
+
+document.querySelector("#start-btn").addEventListener("click", startQuiz);
