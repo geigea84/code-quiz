3837eff017ee75d55e184d4e40157fb8574f2b7e
Geiger <geiga@shopbop.com>
2021-01-12 12:32:28 -0600

    finished quiz functionality

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,18 +1,18 @@
 let questions = [
     {
-        title: "testing: click response a",
-        choices: ["a", "b", "c", "d"],
-        answer: "a"
+        title: "question 1: click response a",
+        options: ["choose a", "choose b", "choose c", "choose d"],
+        answer: "choose a"
     },
     {
-        title: "testing: click response b",
-        choices: ["a", "b", "c", "d"],
-        answer: "b"
+        title: "question 2: click response b",
+        options: ["choose a", "choose b", "choose c", "choose d"],
+        answer: "choose b"
     },
     {
-        title: "testing: click response c",
-        choices: ["a", "b", "c", "d"],
-        answer: "a"
+        title: "question 3: click response c",
+        options: ["choose a", "choose b", "choose c", "choose d"],
+        answer: "choose c"
     }
 ];
 
@@ -26,6 +26,7 @@ const finishEl = document.querySelector("#finish");
 const timerEl = document.querySelector("#timer");
 const questionTitle = document.querySelector("#question-title");
 const responseOptions = document.querySelector("#response-options");
+const finalScore = document.querySelector("#final-score");
 
 function startQuiz() {
     startEl.setAttribute("class", "hide");
@@ -39,29 +40,77 @@ function startQuiz() {
 }
 
 function startQuestions() {
-    //questionsEl.textContent = "";
+    
+    //use questionTitle and responseOptions rather than questionsEl?
+    responseOptions.innerHTML = "";
+
     let currentQuestion = questions[questionIndex]
 
     questionTitle.textContent = currentQuestion.title;
-    responseOptions.textContent = currentQuestion.choices;
-
     
+    currentQuestion.options.forEach(function(option) {
+        let optionButton = document.createElement("button");
+        optionButton.setAttribute("class", "option");
+        optionButton.setAttribute("value", option);
+
+        optionButton.textContent = option;
+
+        optionButton.onclick = answerQuestion;
+
+        responseOptions.appendChild(optionButton);
+    });
+
+    if (time <= 0) {
+        time = 0;
+        endQuiz();
+    }
 }
 
-function checkQuestion() {
+function answerQuestion() {
+    //connects to startQuestions => optionButton.setAttribute("value", option);
+    if (this.value !== questions[questionIndex].answer) {
+        console.log(this.value);
+        console.log("wrong");
+        time = time - 10;
+    }
+    else {
+        console.log(this.value);
+        console.log("correct");
+    }
 
+    questionIndex++
+    console.log(questionIndex);
+
+    if (questionIndex == questions.length) {
+        endQuiz();
+    }
+    else {
+        startQuestions();
+    }
 }
 
 function endQuiz() {
+    clearInterval(timerId);
     questionsEl.setAttribute("class", "hide");
+    timerEl.setAttribute("class", "hide");
     finishEl.removeAttribute("class");
+    console.log(time);
+
+    if (time <= 0) {
+        time = 0;
+    }
+
+    finalScore.textContent = "Your score is " + time;
+    return;
 }
 
 function timerCountdown() {
-    time--;
     timerEl.textContent = time;
-
-    if(time <= 0) {
+    
+    if (time > 0) {
+        time--;
+    }
+    else if(time <= 0) {
         endQuiz();
     }
 }
